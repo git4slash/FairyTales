049ca543e9612f91d89fd60fb9d2d43ede2b2dca
Oleksandr Subbotin <subbotin.al.ig@gmail.com>
2015-09-29 09:27:01 +0200

    merge errors
    delete old dir

--- src/main/java/tales/model/AccountRepository.java
+++
@@ -1,7 +0,0 @@
-package tales.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-
-}
--- src/main/java/tales/model/StoryRepository.java
+++
@@ -1,6 +0,0 @@
-package tales.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StoryRepository extends JpaRepository<Story, Long> {
-}
